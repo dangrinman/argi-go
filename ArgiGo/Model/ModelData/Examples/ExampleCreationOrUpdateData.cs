71c204243b8af63f620b0aab8c932f51047d8765
Daniel A. Grinman <dgrinman@itsynch.com>
2023-10-21 10:39:01 +0900

    chore: clear files

--- ArgiGo/Model/ModelData/Examples/ExampleCreationOrUpdateData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ArgiGo.Model.ModelData.Examples
-{
-    public class ExampleCreationOrUpdateData
-    {
-        public string? Id { get; set; }
-
-        public string? Example { get; set; }
-    }
-}
